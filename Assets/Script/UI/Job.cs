2a966663dce4fdc866567c2ad5be85bda929b09f
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-08-22 18:54:21 +0700

    Add Job scriptable object for job management functionality

---
+++ Assets/Script/UI/Job.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tundayne
+{
+    [CreateAssetMenu(fileName = "Job", menuName = "Tundayne/Job")]
+    public class Job : ScriptableObject
+    {
+        
+    }
+}
+
